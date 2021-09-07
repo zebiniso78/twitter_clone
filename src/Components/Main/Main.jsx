c7d96b2155c3f8329ae3989c8d3dfc258debab58
zebiniso78 <73066830+zebiniso78@users.noreply.github.com>
2021-09-07 05:45:22 +0500

    start of main part

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,9 +1,19 @@
 //Scss
 import './Main.scss';
 
+//Icons
+import Mode from '../Lib/Svg/ChangeMode';
+
 
 function Main() {
     return (
-        
+        <>
+            <div className="main__top-div">
+                <h1 className="main__top-header">Home</h1>
+                <Mode />
+            </div>
+        </>
     );
 }
+
+export default Main;
